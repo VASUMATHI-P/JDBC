e2ac53b5ea4ed613ae3f64072b42bc6ee36175e9
VASUMATHI-P <133555963+VASUMATHI-P@users.noreply.github.com>
2023-05-14 10:13:48 +0530

    Add files via upload

---
+++ railwaydb.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package railwaydb;
+
+
+public class railwaydb {
+    public static void main(String args[]) throws Exception
+	{
+	System.out.println("**************************************************************************************************************");
+	System.out.println("                                      RAILWAY RESERVATION SYSTEM                                              ");
+	System.out.println("**************************************************************************************************************");
+		System.out.println();
+		System.out.println();	
+		railway rail=new railway();
+		rail.main_menu();
+	}		
+    
+}
